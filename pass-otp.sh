43f3adab2902a2d5652c8414ecc7396ac3f3bf37
GOLDCODERS CORP <55337687+goldcoders@users.noreply.github.com>
2021-05-01 07:34:30 +0800

    Update pass-otp.sh

--- pass-otp.sh
+++ pass-otp.sh
@@ -3,17 +3,8 @@
 set -e
 
 QUERY=$1
-PATH=/opt/homebrew/bin:$PATH
-
-# GPG agent
-envfile="$HOME/.gnupg/gpg-agent.env"
-if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
-    eval "$(cat "$envfile"); export GPG_AGENT_INFO"
-else
-    eval "$(gpg-agent --daemon --write-env-file "$envfile")"
-fi
-
+PATH=/opt/homebrew/bin/:$PATH
 # PASS
-#pass otp "$QUERY" | awk 'BEGIN{ORS=""} {print; exit}' | pbcopy
-pass otp -c "$QUERY"
-osascript -e 'display notification "Copied OTP key to clipboard" with title "Unix pass"'
+PINENTRY_USER_DATA=gui pass otp "$QUERY" | awk 'BEGIN{ORS=""} {print; exit}'
+osascript -e 'display notification "Copied OTP key to clipboard" with title "OTP"'
+
