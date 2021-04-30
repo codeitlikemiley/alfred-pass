bbb80744aa917b924df0fa1dcc7a4031368250ad
GOLDCODERS CORP <55337687+goldcoders@users.noreply.github.com>
2021-05-01 07:34:05 +0800

    Update pass-generate.sh

--- pass-generate.sh
+++ pass-generate.sh
@@ -3,6 +3,6 @@
 set -e
 
 QUERY=$1
-PATH=/opt/homebrew/bin:$PATH
-
-pass generate "$QUERY" -n 20 -c 
+PATH=/opt/homebrew/bin/:$PATH
+PINENTRY_USER_DATA=gui pass generate "$QUERY" | tail -n 1 | perl -pe 's/\e\[[0-9;]*m//g'
+osascript -e 'display notification "Copied New Password to clipboard" with title "New pass"'
